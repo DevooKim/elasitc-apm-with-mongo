e4fc761af47f54f20e277514258c4b932d577435
Devookim <khwoo8026@gmail.com>
2023-12-11 03:16:36 +0900

    third commit

--- index.js
+++ index.js
@@ -15,13 +15,17 @@ if (apm.isStarted()) {
 }
 
 mongoose.set('debug', (collectionName, method, query, doc) => {
+    // 조건
+    // 1. collectionName이 users
+    // 2. userId가 있을 경우
+    // 3. 조회일 경우
     if (collectionName === 'users' && apm.isStarted()) {
 
         const user = apm.currentTransaction?._user || {}
         const custom = apm.currentTransaction?._custom || {}
         const transactionId = apm.currentTraceIds['transaction.id']
 
-        //pino로 적당히
+        //pino로 적당히 -> stream으로 보내면 될듯
         console.log(`${JSON.stringify(custom)}, ${JSON.stringify(user)}, ${transactionId}, ${collectionName}.${method}`, JSON.stringify(query), doc);
         //{"hello":"world"}, {"id":"123","username":"123","email":"123"}, 028db2efed1ce952, users.find {"id":1} { sort: { _id: -1 } }
     }
